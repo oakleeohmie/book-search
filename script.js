1a662e0038277562926ed54f32091aed30baae11
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-01-04 11:30:58 -0700

    created clear button function

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var isbnTerm = $("#search-ISBN").val().trim();
 
 
 var nybook = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?"
+
+function clear() {
+    $("#results-section").empty();
+};
+$('#run-search').on('click', function (event) {
+    event.preventDefault();
+    clear();
+    var url = buildQueryURL();
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(updatePage)
