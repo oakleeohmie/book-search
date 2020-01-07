26aabdbbd294ecc415743d05c7828f73f98d077e
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-01-06 20:40:02 -0700

    altered url in ajax

--- script.js
+++ script.js
@@ -3,19 +3,15 @@ var isbnTerm = $("#search-ISBN").val().trim();
 var genTerm = $("#search-genre").val().trim();
 
 function buildNewYorkURL() {
-
-
     var nyBook = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?"
-
-    var url = nyBook + "author=" + authTerm + "&isbn=" + isbnTerm + "&genre=" + genTerm + "&api-key=Elscl5JmJnrLqqoCMZB0BAkVXEoApHOU";
+    var url = nyBook + "author=" + authTerm + "&isbn=" + isbnTerm + "&api-key=Elscl5JmJnrLqqoCMZB0BAkVXEoApHOU";
     return (url);
 };
 function buildGoodReadsURL() {
     var goodReads = "https://www.goodreads.com/search.xml?"
     var url = goodReads + "q=" + authTerm + isbnTerm + "slug=" + genTerm + "&key=506nRRgeF7qmVFcKIaro9g&";
     return (url);
-}
-
+};
 function clear() {
     $("#results-section").empty();
 };
