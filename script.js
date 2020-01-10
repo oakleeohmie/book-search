c8d6354f2964293d04d62cbb729eec915620732d
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-01-09 19:53:29 -0700

    accessing title from api data object

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-var authTerm = $("#search-author").val().trim();
-var isbnTerm = $("#search-ISBN").val().trim();
-var genTerm = $("#search-genre").val().trim();
-
 function buildNewYorkURL() {
-    var nyBook = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?"
-    var url = nyBook + "author=" + authTerm + "&api-key=Elscl5JmJnrLqqoCMZB0BAkVXEoApHOU";
+    var authTerm = $("#search-author").val().trim();
+    var titleTerm = $("#search-title").val().trim();
+    var isbnTerm = $("#search-ISBN").val().trim();
+    var genTerm = $("#search-genre").val().trim();
+    var nyBook = "https://api.nytimes.com/svc/books/v3/reviews.json?"
+    var url = nyBook + "author=" + authTerm + "&title=" + titleTerm + "&api-key=Elscl5JmJnrLqqoCMZB0BAkVXEoApHOU";
     return (url);
 };
-function buildGoodReadsURL() {
-    var goodReads = "https://www.goodreads.com/search.xml?"
-    var url = goodReads + "q=" + authTerm + "&key=506nRRgeF7qmVFcKIaro9g";
-    return (url);
-};
-function updatePage() {
-    var results = response.docs[i]
-    var title = results.
+function updatePage(bookData) {
+    console.log(bookData)
+    var book = bookData.response;
+    var $bookList = $("<ul>");
+    $bookList.addClass("list-group");
+    $("#results-section").append($bookList);
+    var title = book.results.book_title;
+    console.log(title)
+    var $bookListItem = $("<li class='list-group-item bookTitle'>");
+    if (title) {
+        $bookListItem.append(
+            "<span class='label label-primary'>" +
+            title +
+            "</strong>"
+        );
+    }
+
 }
 function clear() {
     $("#results-section").empty();
