b5eb589f20fe4532627998aef7c85f064886d4d3
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-01-11 09:42:54 -0700

    changed url format to JSON

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ function buildNewYorkURL() {
     var authTerm = $("#search-author").val().trim();
     var titleTerm = $("#search-title").val().trim();
     var isbnTerm = $("#search-ISBN").val().trim();
-    var genTerm = $("#search-genre").val().trim();
     var nyBook = "https://api.nytimes.com/svc/books/v3/reviews.json?"
     var url = nyBook + "author=" + authTerm + "&title=" + titleTerm + "&isbn=" + isbnTerm + "&api-key=Elscl5JmJnrLqqoCMZB0BAkVXEoApHOU";
     return (url);
 };
-function updatePage(bookData) {
+function buildDreambooksURL() {
+    var authTerm = $("#search-author").val().trim();
+    var titleTerm = $("#search-title").val().trim();
+    var genTerm = $("#search-genre").val().trim();
+    var dreamBooks = "http://idreambooks.com/api/books/reviews.json?"
+    var url = dreamBooks + "q=" + authTerm + titleTerm + "&slug=" + genTerm + "&key=4a62088f871d332635a4513570e122bae72bf036";
+    return (url);
+};
+function updatePage1(bookData) {
     for (i = 0; i < bookData.results.length; i++) {
         console.log(bookData)
         var book = bookData.results[i];
@@ -16,7 +23,7 @@ function updatePage(bookData) {
         $("#results-section").append($bookList);
         var author = book.book_author;
         console.log(author)
-        var $bookListItem = $("<li class='list-group-item bookTitle'>");
+        var $bookListItem = $("<li class='list-group-item'>");
         if (author) {
             $bookListItem.append(
                 "<span class='label label-primary'>" +
@@ -36,9 +43,34 @@ function updatePage(bookData) {
         };
         var isbn = book.isbn13;
         if (isbn) {
-            $bookListItem.append("<p> ISBN: " + isbn + "<p>");
+            $bookListItem.append("<p> ISBN: " + isbn + " <p>");
         };
         $bookList.append($bookListItem);
+    };
+};
+function updatePage2(bookData2) {
+    for (i = 0; i < bookData2.GoodreadsResponse.search.results.work.best_book.length; i++) {
+        console.log(bookData2)
+        var book2 = bookData2.GoodreadsResponse.search.results.work.best_book[i];
+        var $bookList = $("<ul>");
+        $bookList.addClass("list-group");
+        $("#results-section").append($bookList);
+        var author = book2.author.name;
+        console.log(author)
+        var $bookListItem = $("<li class='list-group-item'>");
+        if (author) {
+            $bookListItem.append(
+                "<span class='label label-primary'>" +
+                author +
+                "</strong>"
+            );
+        };
+        var title = book2.title;
+        if (title) {
+            console.log(title);
+            $bookListItem.append("<h3>" + title + "<h3>");
+
+        };
     };
 };
 function clear() {
@@ -51,5 +83,14 @@ $('#run-search').on('click', function (event) {
     $.ajax({
         url: NYurl,
         method: "GET"
-    }).then(updatePage);
+    }).then(updatePage1);
+});
+$('#run-search').on('click', function (event) {
+    event.preventDefault();
+    clear();
+    var GRurl = buildGoodreadsURL();
+    $.ajax({
+        url: GRurl,
+        method: "GET"
+    }).then(updatePage2);
 });
